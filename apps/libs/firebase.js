01cd2723ea3845158f13d01ece1b5289224b359e
muhyidin3222 <muhyidin480@gmail.com>
2022-02-20 21:15:49 +0700

    remove google firebase

--- apps/libs/firebase.js
+++ apps/libs/firebase.js
@@ -1,7 +1,8 @@
 
 const admin = require("firebase-admin");
 const axios = require('axios')
-const serviceAccount = require("../../core/bisaekspor-8ff80-d115115d4338.json");
+const serviceAccount = {}
+//  require("../../core/bisaekspor-8ff80-d115115d4338.json");
 
 axios.defaults.headers.common['Authorization'] = "key=" + process.env.FCMFIREBASE;
 
